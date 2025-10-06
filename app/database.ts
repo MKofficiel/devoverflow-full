31f635c380490c9ea734e42879ba17f28ee4ee16
Mohamed <mohamedkondombo135@gmail.com>
2025-10-05 21:00:14 -0400

    Implement Create &  Read User Routes

---
+++ app/database.ts
@@ -0,0 +1,58 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix nex.js API Route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Add authentication",
+    status: "in progress",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Improve UI design",
+    status: "completed",
+    type: "improvement",
+  },
+  {
+    id: 4,
+    name: "Optimize database queries",
+    status: "open",
+    type: "improvement",
+  },
+  {
+    id: 5,
+    name: "Implement logging",
+    status: "in progress",
+    type: "feature",
+  },
+  {
+    id: 6,
+    name: "Set up CI/CD pipeline",
+    status: "completed",
+    type: "feature",
+  },
+  {
+    id: 7,
+    name: "Fix responsive design issues",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 8,
+    name: "Update dependencies",
+    status: "completed",
+    type: "maintenance",
+  },
+  {
+    id: 9,
+    name: "Add unit tests",
+    status: "in progress",
+    type: "feature",
+  },
+];
+
+export default tickets;
